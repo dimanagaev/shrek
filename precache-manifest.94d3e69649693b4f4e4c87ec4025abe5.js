15a08c837f3ee40bf357fe8f5598477b23fdae82
dimanagaev <59776707+dimanagaev@users.noreply.github.com>
2020-01-12 11:53:59 +0300

    ........

---
+++ precache-manifest.94d3e69649693b4f4e4c87ec4025abe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637eb3d6c4ae1083cd6bae1899691f0f",
+    "url": "/shrek/index.html"
+  },
+  {
+    "revision": "80b2529715fed297cc09",
+    "url": "/shrek/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "138c560089278946abf5",
+    "url": "/shrek/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "80b2529715fed297cc09",
+    "url": "/shrek/static/js/2.552e587f.chunk.js"
+  },
+  {
+    "revision": "138c560089278946abf5",
+    "url": "/shrek/static/js/main.5e255508.chunk.js"
+  },
+  {
+    "revision": "0513303425e1f278300c",
+    "url": "/shrek/static/js/runtime-main.624a7dc1.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/shrek/static/media/persik.4e1ec840.png"
+  }
+]);
